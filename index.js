71293101cb6eb6725d608883a724f8b5ea957534
Clayton Leach <clayton.leach@outlook.com>
2018-12-11 19:33:30 -0500

    First setup for heroku

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Routes
 // ===========================================================
 app.get("/", function(req, res) {
-  res.send("Something.");
+  res.send(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, function() {
